1d317f51b949d69933c9f2ea71046293777e35ba
Adile <adile.sellami@gmail.com>
2022-01-08 21:12:30 +0100

    update

--- launchJenkinsDocker.sh
+++ launchJenkinsDocker.sh
@@ -1,4 +1,3 @@
-docker run -p 8080:8080 -p 50000:50000 \
+docker run -d -p 8080:8080 -p 50000:50000 \
 -v jenkins_home2:/var/jenkins_home \
--v /var/run/docker.sock:/var/run/docker.sock \
 --name jenkins2 jenkins/jenkins:lts
